01314beb60863a6548ea694a706551bdede25f49
DavyHyun <dhyun1124@gmail.com>
2024-03-14 07:34:26 -0700

    user login db

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const dotenv = require("dotenv");
 const notes = require("./data/notes");
 var cors = require("cors");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
 connectDB();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
@@ -19,10 +22,15 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
+app.use("/api/users", userRoutes);
 
-  res.send(note);
-});
+app.use(notFound);
+app.use(errorHandler);
+
+// app.get("/api/notes/:id", (req, res) => {
+//   const note = notes.find((n) => n._id === req.params.id);
+
+//   res.send(note);
+// });
 
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
