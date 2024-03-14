08b1a09bf70f28563336ea1a3c3a66d06fd013c8
DavyHyun <dhyun1124@gmail.com>
2024-03-14 07:44:35 -0700

    userAuth done backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const generateToken = require("../utils/generateToken");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
@@ -24,6 +25,7 @@ const registerUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       email: user.email,
       pic: user.pic,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -43,6 +45,7 @@ const authUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       email: user.email,
       pic: user.pic,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
